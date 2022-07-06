ff5328891c1a010df8bc02a5be1d429add7baaa1
Jenya Cherny <jenya.cherny@gmail.com>
2022-07-05 20:18:04 -0400

    Updated error logging feature and json file format after adding a new wine

--- Program.cs
+++ Program.cs
@@ -5,10 +5,22 @@ using System.Collections.Generic;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void ErrorLogging(string error) //Handles any errors when a user choses invalid menu option and logs them 
     {
 
-        string FilePath = @"./WineDescription.json";
+        using (StreamWriter W = File.AppendText(@"./Error.txt"))
+        {
+            W.WriteLine(error);
+        }
+        Console.WriteLine("An error is logged");
+       
+    }
+
+    public static void Main(string[] args)
+    {
+            using (File.Create(@"./Error.txt")) { }//Creating a new error file to log and closing it
+
+            string FilePath = @"./WineDescription.json";
         string ReadText = File.ReadAllText(FilePath);
 
         List<Class1> inventory = System.Text.Json.JsonSerializer.Deserialize<List<Class1>>(ReadText);
@@ -22,14 +34,10 @@ public class Program
             Console.WriteLine("4. Exit \n");
             menuchoice = Console.ReadLine();
 
-            // if (menuchoice > 4 || menuchoice <= 0)
-            //{
-            //    Console.WriteLine("Please choose from option 1 to 4")
-            //}
+            
 
             Console.WriteLine("\n You have chosen menu option #" + menuchoice);
-            //Add code to call menu option #1 here
-            //Add code to call menu option #2 here
+                    
 
             if (menuchoice == "3")
             {
@@ -47,10 +55,10 @@ public class Program
                     if (item.WineName == UserWineChoice)
                     {
                         Console.WriteLine("Wine Name: " + item.WineName);
-                        Console.WriteLine(item.Description);
-                        Console.WriteLine(item.Quantity);
-                        Console.WriteLine(item.SupplierName);
-                        Console.WriteLine(item.Price);
+                        Console.WriteLine("Description: " + item.Description);
+                        Console.WriteLine("Quantity: " + item.Quantity);
+                        Console.WriteLine("Supplier Name: " + item.SupplierName);
+                        Console.WriteLine ("Price: " + item.Price);
                     }
                 }
             }
@@ -73,12 +81,18 @@ public class Program
                 Console.WriteLine("Please enter Price");
                 item.Price = Console.ReadLine();
 
-                inventory.Add(item);
-                var json = System.Text.Json.JsonSerializer.Serialize<List<Class1>>(inventory);
+                inventory.Add(item);//Adds a new wine to json file and shows in inventory list on next inventory status
+                System.Text.Json.JsonSerializerOptions options = new System.Text.Json.JsonSerializerOptions();
+                options.WriteIndented = true;
+                var json = System.Text.Json.JsonSerializer.Serialize<List<Class1>>(inventory, options);
                 File.WriteAllText(FilePath, json);
             }
-
-            } while (menuchoice != "4");
+            else if(menuchoice !="4")
+            {
+                Console.WriteLine("Please enter a valid menu choice: 1,2,3 or 4");
+               ErrorLogging(menuchoice);
+            }
+         } while (menuchoice != "4");
 
 
       
